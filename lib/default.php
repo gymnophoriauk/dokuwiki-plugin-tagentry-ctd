6c8f6561a84a5f4a88a5200c78c481e55e7c338c
gymnophoriauk <robertdhow@gmail.com>
2026-05-27 13:05:57 +0100

    Create default.php

---
+++ lib/default.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the tagentry Plugin
+ */
+$conf['tagsrc']       = 'All tags'; // where to get the tags from
+$conf['namespace']    = 'tag';      // scan here for tags
+$conf['limit']        = 0;          // 0: unlimited
+$conf['blacklist']    = 'start';    // space separated list
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
