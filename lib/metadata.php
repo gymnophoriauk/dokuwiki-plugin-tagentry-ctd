4a376743b385f1c7a9a3e7d65d4ae23488c456c6
gymnophoriauk <robertdhow@gmail.com>
2026-05-27 13:06:31 +0100

    Create metadata.php

---
+++ lib/metadata.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Metadata for configuration manager plugin
+ * Additions for the tagentry plugin
+ *
+ * @author    Robin Gareus <robin@gareus.org>
+ */
+$meta['tagsrc']       = array('multichoice', '_choices' => array('All tags','Pagenames in tag NS', 'Pages in given namespace'));
+$meta['namespace']    = array('string');
+$meta['blacklist']    = array('string');
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
